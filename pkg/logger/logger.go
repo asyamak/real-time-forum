243cdf9300a756e676e12d1ec4f3e2fb3a6356d1
rshezarr <rshezarr@gmail.ru>
2023-01-18 18:58:59 +0600

    add: logger

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,1 +1,27 @@
 package logger
+
+import (
+	"fmt"
+	"os"
+)
+
+type Logger struct {
+	prefix string
+}
+
+func NewLogger(prefix string) *Logger {
+	return &Logger{prefix: prefix}
+}
+
+func (l *Logger) Info(format string, v ...interface{}) {
+	l.log("\033[34m[INFO]\033[0m", format, v...)
+}
+
+func (l *Logger) Error(format string, v ...interface{}) {
+	l.log("\033[31m[ERROR]\033[0m", format, v...)
+	os.Exit(1)
+}
+
+func (l *Logger) log(prefix, format string, v ...interface{}) {
+	fmt.Printf("\033[32m%s\033[0m %s: %s\n", l.prefix, prefix, fmt.Sprintf(format, v...))
+}
