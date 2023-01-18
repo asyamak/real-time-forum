c26ab3362a5b995f4be2fe4897d74600d87ed3d3
rshezarr <rshezarr@gmail.ru>
2023-01-18 18:59:27 +0600

    add: connect db

--- pkg/sqlite/sqlite.go
+++ pkg/sqlite/sqlite.go
@@ -1,1 +1,25 @@
 package sqlite
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+func ConnectDB(driver, dbName, schemePath string) (*sql.DB, error) {
+	enableForeignKeys := "?_foreign_keys=on&cache=shared&mode=rwc"
+
+	db, err := sql.Open(driver, dbName+enableForeignKeys)
+	if err != nil {
+		return nil, fmt.Errorf("open database: %w", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("ping database: %w", err)
+	}
+
+	if err := createTables(db, schemePath); err != nil {
+		return nil, fmt.Errorf("connect db: %w", err)
+	}
+
+	return db, nil
+}
