a2a64b3a8c0fd9f1556b513efd0c21c058447163
rshezarr <rshezarr@gmail.ru>
2023-01-18 18:58:00 +0600

    add: handler router

--- internal/handler/http/handler.go
+++ internal/handler/http/handler.go
@@ -1,1 +1,23 @@
 package http
+
+import (
+	"real-time-forum/internal/service"
+
+	"github.com/rshezarr/gorr"
+)
+
+type Handler struct {
+	service *service.Service
+}
+
+func NewHandler(service *service.Service) *Handler {
+	return &Handler{
+		service: service,
+	}
+}
+
+func (h *Handler) InitRoutes() *gorr.Router {
+	router := gorr.NewRouter()
+
+	return router
+}
