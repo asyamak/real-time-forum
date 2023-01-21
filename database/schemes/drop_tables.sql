4dcbc403e375068acf1de68598d79a1cb53e6bdf
rshezarr <rshezarr@gmail.com>
2023-01-21 18:05:04 +0600

    fix: drop schemes order

--- database/schemes/drop_tables.sql
+++ database/schemes/drop_tables.sql
@@ -1,9 +1,17 @@
-DROP TABLE user,
-post,
-comment,
-categories,
-post_categories,
-vote_post,
-vote_comment,
-post_images,
-session_tokens;
+DROP TABLE post_categories;
+
+DROP TABLE session_tokens;
+
+DROP TABLE post_images;
+
+DROP TABLE vote_comment;
+
+DROP TABLE vote_post;
+
+DROP TABLE categories;
+
+DROP TABLE comment;
+
+DROP TABLE post;
+
+DROP TABLE user;
