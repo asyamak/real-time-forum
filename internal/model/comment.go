44a932f87e90383ebf1635fa895e0a94161c713a
rshezarr <rshezarr@gmail.com>
2023-01-21 13:16:47 +0600

    add: comment model

--- internal/model/comment.go
+++ internal/model/comment.go
@@ -1,4 +1,12 @@
 package model
 
 type Comment struct {
+	ID           int         `json:"id"`
+	Author       User        `json:"author"`
+	PostID       int         `json:"postID"`
+	Content      string      `json:"content"`
+	ImagePath    string      `json:"image_path"`
+	CreationTime interface{} `json:"creation_time"`
+	UserRate     int         `json:"userRate"`
+	Rating       int         `json:"rating"`
 }
