f596cccfaa073efc1528ecdff5510b4b2b3093a0
rshezarr <rshezarr@gmail.com>
2023-01-21 13:14:58 +0600

    add: post model

--- internal/model/post.go
+++ internal/model/post.go
@@ -1,4 +1,14 @@
 package model
 
 type Post struct {
+	ID           int         `json:"id"`
+	Author       User        `json:"author"`
+	Title        string      `json:"title"`
+	Content      string      `json:"content"`
+	CreationTime interface{} `json:"creation_time"`
+	ImagePath    string      `json:"image_path"`
+	Categories   []Category  `json:"categories"`
+	Comments     []Comment   `json:"comments"`
+	Rating       int         `json:"rating"`
+	UserRate     int         `json:"user_rate"`
 }
