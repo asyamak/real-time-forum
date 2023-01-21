c710a2781a846a3ec628fdab7d9e32438930e770
rshezarr <rshezarr@gmail.com>
2023-01-21 13:45:32 +0600

    update: user model

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,14 +1,14 @@
 package model
 
 type User struct {
-	ID         int         `json:"id"`
-	Email      string      `json:"email"`
-	Username   string      `json:"username"`
-	Password   string      `json:"-"`
-	FirstName  string      `json:"firstName"`
-	LastName   string      `json:"lastName"`
-	Age        int         `json:"age"`
-	Gender     string      `json:"gender"`
-	Registered interface{} `json:"registered"`
-	Avatar     string      `json:"avatar"`
+	ID           int         `json:"id"`
+	Email        string      `json:"email"`
+	Username     string      `json:"username"`
+	Password     string      `json:"-"`
+	FirstName    string      `json:"firstName"`
+	LastName     string      `json:"lastName"`
+	Age          int         `json:"age"`
+	Gender       string      `json:"gender"`
+	CreationTime interface{} `json:"registered"`
+	Avatar       string      `json:"avatar"`
 }
