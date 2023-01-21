84fa5a8ca0bbf57a1f28987ff1d146898be95c0e
rshezarr <rshezarr@gmail.com>
2023-01-21 13:22:56 +0600

    add: chat model

--- internal/model/chat.go
+++ internal/model/chat.go
@@ -1,4 +1,7 @@
 package model
 
 type Chat struct {
+	User                User    `json:"user"`
+	LastMessage         Message `json:"last_message"`
+	UnreadMessagesCount int     `json:"unread_messages_count"`
 }
