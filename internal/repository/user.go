cf520cefd9b9f03a7b8c754687be5e0e8f75508b
rshezarr <rshezarr@gmail.com>
2023-01-21 13:11:24 +0600

    add: user methods repo

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -1,4 +1,13 @@
 package repository
 
+import "real-time-forum/internal/model"
+
 type User interface {
+	Create(user model.User) error
+	GetByCredentials(usernameOrEmail, password string) (model.User, error)
+	GetByID(userID int) (model.User, error)
+	GetUsersPosts(userID int) ([]model.Post, error)
+	GetUsersRatedPosts(userID int) ([]model.Post, error)
+	SetSession(session model.Session) error
+	DeleteSession(userID int, refreshToken string) error
 }
