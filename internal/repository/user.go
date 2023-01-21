69e63bb1b781263dae7f8e77577510b038aaa2f5
rshezarr <rshezarr@gmail.com>
2023-01-21 13:38:31 +0600

    implement: user interface repo

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -24,3 +24,31 @@ func NewUser(db *sql.DB) *UserRepository {
 		db: db,
 	}
 }
+
+func (r *UserRepository) Create(user model.User) error {
+	panic("not implemented") // TODO: Implement
+}
+
+func (r *UserRepository) GetByCredentials(usernameOrEmail string, password string) (model.User, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (r *UserRepository) GetByID(userID int) (model.User, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (r *UserRepository) GetUsersPosts(userID int) ([]model.Post, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (r *UserRepository) GetUsersRatedPosts(userID int) ([]model.Post, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (r *UserRepository) SetSession(session model.Session) error {
+	panic("not implemented") // TODO: Implement
+}
+
+func (r *UserRepository) DeleteSession(userID int, refreshToken string) error {
+	panic("not implemented") // TODO: Implement
+}
