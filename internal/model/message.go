8756405da440ef3361f06d641df0882690c20979
rshezarr <rshezarr@gmail.com>
2023-01-21 13:21:36 +0600

    add: message model

--- internal/model/message.go
+++ internal/model/message.go
@@ -1,4 +1,10 @@
 package model
 
 type Message struct {
+	ID           int         `json:"id"`
+	SenderID     int         `json:"sender_id"`
+	RecipientID  int         `json:"recipient_id"`
+	Message      string      `json:"message"`
+	CreationTime interface{} `json:"creation_time"`
+	Readed       bool        `json:"readed"`
 }
