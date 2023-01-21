dcca13ab5b958712a16b07e8ff45f3afc9e08893
rshezarr <rshezarr@gmail.com>
2023-01-21 13:39:45 +0600

    add: context as argument, user repo

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -1,18 +1,19 @@
 package repository
 
 import (
+	"context"
 	"database/sql"
 	"real-time-forum/internal/model"
 )
 
 type User interface {
-	Create(user model.User) error
-	GetByCredentials(usernameOrEmail, password string) (model.User, error)
-	GetByID(userID int) (model.User, error)
-	GetUsersPosts(userID int) ([]model.Post, error)
-	GetUsersRatedPosts(userID int) ([]model.Post, error)
-	SetSession(session model.Session) error
-	DeleteSession(userID int, refreshToken string) error
+	Create(ctx context.Context, user model.User) error
+	GetByCredentials(ctx context.Context, usernameOrEmail, password string) (model.User, error)
+	GetByID(ctx context.Context, userID int) (model.User, error)
+	GetUsersPosts(ctx context.Context, userID int) ([]model.Post, error)
+	GetUsersRatedPosts(ctx context.Context, userID int) ([]model.Post, error)
+	SetSession(ctx context.Context, session model.Session) error
+	DeleteSession(ctx context.Context, userID int, refreshToken string) error
 }
 
 type UserRepository struct {
@@ -25,30 +26,30 @@ func NewUser(db *sql.DB) *UserRepository {
 	}
 }
 
-func (r *UserRepository) Create(user model.User) error {
+func (r *UserRepository) Create(ctx context.Context, user model.User) error {
 	panic("not implemented") // TODO: Implement
 }
 
-func (r *UserRepository) GetByCredentials(usernameOrEmail string, password string) (model.User, error) {
+func (r *UserRepository) GetByCredentials(ctx context.Context, usernameOrEmail string, password string) (model.User, error) {
 	panic("not implemented") // TODO: Implement
 }
 
-func (r *UserRepository) GetByID(userID int) (model.User, error) {
+func (r *UserRepository) GetByID(ctx context.Context, userID int) (model.User, error) {
 	panic("not implemented") // TODO: Implement
 }
 
-func (r *UserRepository) GetUsersPosts(userID int) ([]model.Post, error) {
+func (r *UserRepository) GetUsersPosts(ctx context.Context, userID int) ([]model.Post, error) {
 	panic("not implemented") // TODO: Implement
 }
 
-func (r *UserRepository) GetUsersRatedPosts(userID int) ([]model.Post, error) {
+func (r *UserRepository) GetUsersRatedPosts(ctx context.Context, userID int) ([]model.Post, error) {
 	panic("not implemented") // TODO: Implement
 }
 
-func (r *UserRepository) SetSession(session model.Session) error {
+func (r *UserRepository) SetSession(ctx context.Context, session model.Session) error {
 	panic("not implemented") // TODO: Implement
 }
 
-func (r *UserRepository) DeleteSession(userID int, refreshToken string) error {
+func (r *UserRepository) DeleteSession(ctx context.Context, userID int, refreshToken string) error {
 	panic("not implemented") // TODO: Implement
 }
