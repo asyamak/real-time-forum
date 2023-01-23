3cb6aeeaab4df42d8d49704ea62cb4503f30c9b9
rshezarr <rshezarr@gmail.ru>
2023-01-23 20:46:25 +0600

    implement: user service

--- internal/service/user.go
+++ internal/service/user.go
@@ -35,7 +35,31 @@ type UserSignUpInput struct {
 	Password  string
 }
 
+func (s *UserService) SignUp(ctx context.Context, input UserSignUpInput) error {
+	panic("not implemented") // TODO: Implement
+}
+
 type UserSignInInput struct {
 	UsernameOrEmail string
 	Password        string
+}
+
+func (s *UserService) SignIn(ctx context.Context, input UserSignInInput) (string, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (s *UserService) GetByID(ctx context.Context, userID int) (model.User, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (s *UserService) GetUsersPosts(ctx context.Context, userID int) ([]model.Post, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (s *UserService) GetUsersVotedPosts(ctx context.Context, userID int) ([]model.Post, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (s *UserService) SetToken(ctx context.Context, userID int) (string, error) {
+	panic("not implemented") // TODO: Implement
 }
