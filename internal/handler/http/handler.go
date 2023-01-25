636f52ccb18347809cc83a514f05a178527612a4
rshezarr <rshezarr@gmail.com>
2023-01-25 20:13:34 +0600

    add: comment for handler

--- internal/handler/http/handler.go
+++ internal/handler/http/handler.go
@@ -18,12 +18,22 @@ func NewHandler(service *service.Service) *Handler {
 
 func (h *Handler) InitRoutes() *gorr.Router {
 	router := gorr.NewRouter()
-
+	// user handlers
 	router.POST("/api/user/sign-up", h.SignUp)
 	router.POST("/api/user/sign-in", h.SignIn)
 	router.GET("/api/user/:user_id", h.GetUser)
 	router.GET("/api/user/:user_id/posts", h.GetUserPosts)
 	router.GET("/api/user/:user_id/liked-posts", h.GetUserVotedPosts)
+
+	//post handlers
+
+	//categories handlers
+
+	//comments handlers
+
+	//chat handlers
+
+	//images fileserver
 
 	return router
 }
