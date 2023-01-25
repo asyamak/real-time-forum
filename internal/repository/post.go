f411a372c554a7f68f7511163e2ad9b463acfaf2
rshezarr <rshezarr@gmail.com>
2023-01-25 20:18:56 +0600

    implement: post methods repo

--- internal/repository/post.go
+++ internal/repository/post.go
@@ -23,3 +23,27 @@ func NewPost(db *sql.DB) *PostRepository {
 		db: db,
 	}
 }
+
+func (r *PostRepository) Create(post model.Post) (int, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (r *PostRepository) GetByID(postID int, userID int) (model.Post, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (r *PostRepository) Delete(userID int, postID int) error {
+	panic("not implemented") // TODO: Implement
+}
+
+func (r *PostRepository) GetPostsByCategoryID(categoryID int, limit int, offset int) ([]model.Post, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (r *PostRepository) LikePost(like model.PostVotes) (bool, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (r *PostRepository) DislikePost(dislike model.PostVotes) (bool, error) {
+	panic("not implemented") // TODO: Implement
+}
